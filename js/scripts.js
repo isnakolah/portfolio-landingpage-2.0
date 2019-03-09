053f4ffb735f1ee6263c2d3842cffc2eb12a5ce7
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-03-09 18:56:55 +0300

    add animations on buttons shaking

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 //Business Logic
-function theme1() {
+function specialTheme() {
   $('#doc').addClass('theme-2');
   $('#portfolio, #story, #blog').addClass('button-config-2');
   $('#welcome-text').addClass('welcome-2').removeClass('welcome');
 }
 //Front-end
 $(document).ready(function () {
-  $('#blog').click(function () {
-    theme1();
+  $('#special').click(function () {
+    specialTheme();
   });
 });
 
