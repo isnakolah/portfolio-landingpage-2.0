28e5d85ca9afcee8e88e3f34a3dbbf7c16a30d1b
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-03-09 22:43:04 +0300

    organise workspace

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,18 @@ function specialTheme() {
   $('#doc').addClass('theme-2');
   $('#portfolio, #story, #blog').addClass('button-config-2');
   $('#welcome-text').addClass('welcome-2').removeClass('welcome');
+  $('#dropdown').addClass('.btn-config-2, .btn');
+}
+
+function editorTheme() {
+
 }
 //Front-end
 $(document).ready(function () {
   $('#special').click(function () {
     specialTheme();
   });
-});
+  $('#editor').click(function () {
 
+  });
+});
